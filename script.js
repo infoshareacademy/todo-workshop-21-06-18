c5d49770f54c0bebe1ebe06014a6be49a20d515c
Mateusz Choma <mateusz@amazingdesign.eu>
2018-06-21 20:41:07 +0200

    Event listeners added to input and button

--- script.js
+++ script.js
@@ -10,7 +10,31 @@ class ToDo {
     }
 
     render(){
+        this.toDoListContainer.innerHTML = ''
 
+        const input = document.createElement('input')
+        const button = document.createElement('button')
+        button.innerHTML = 'Dodaj zadanie!'
+
+        input.addEventListener(
+            'input',
+            (event) => this.onNewTaskNameChanged(event)
+        )
+        button.addEventListener(
+            'click',
+            () => this.onAddNewTaskClickHandler()
+        )
+
+        this.toDoListContainer.appendChild(input)
+        this.toDoListContainer.appendChild(button)
+    }
+
+    onAddNewTaskClickHandler(){
+        console.log('bu!')
+    }
+
+    onNewTaskNameChanged(event){
+        console.log(event.target.value)
     }
 
     addTask(taskName) {
